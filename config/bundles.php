863b103a6d6ad1aa114fe955e4f25d4b88813d89
Lamine Diarra <laminecoder@gmail.com>
2024-09-14 22:47:51 +0000

    Feat: Add Turbo

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,6 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
